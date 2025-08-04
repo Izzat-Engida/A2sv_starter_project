f88a08bb8316f89b27d9eda3f3c47e4cb9bbddfa
Izzat Engida <izzatengida7@gmail.com>
2025-08-04 15:22:33 +0300

    more progress on the login page

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -3,12 +3,21 @@ import { User } from '@/types/globaltype'
 import React from 'react'
 import { useForm} from 'react-hook-form'
 import { signIn } from 'next-auth/react'
-
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import {FaLock} from 'react-icons/fa'
 function SignIN() {
   const {register,handleSubmit,formState}=useForm<User>()
   const {errors}=formState
   return (
-    <div className='text-white' onSubmit={handleSubmit(async(data:User)=>{
+    <div className='h-[25%] w-[50%]' >
+      <div>
+      <div>logo placment</div>
+      <div className='text-[40px]'>Sign in to your account</div>
+      <div className='text-[#4F46E5]'><span>Back to Home</span>|<span>Create a new applicant account</span></div>
+      </div>
+      <div>
+      <form  onSubmit={handleSubmit(async(data:User)=>{
       const res=await signIn('credentials',{
         email:data.email,
         password:data.password,
@@ -17,13 +26,25 @@ function SignIN() {
       })
         //need help with the logic here so don't forget to correct it
     })}>
-      asdkjnkjdsnf
-      <form>
-
-      <input type='text' {...register("email")}/> 
-      <input type='text' {...register("password")}/>
-      <button type="submit">submit</button>
+      <div>
+      <div>
+        <Input type="email" {...register("email")}/>
+      </div>
+      <div>
+        <Input type="password" {...register("password")}/>
+      </div>
+      </div>
+      <div className='flex justify-between my-[5px]'>
+        <span><input type='checkbox'/> <span className='ml-[5px]'>Remember me</span></span>
+       <span>Forgot your password?</span>
+      </div>
+      <Button className='w-[100%] bg-[#4F46E5] flex justify-between :hover:bg-[#4F46E5] text-white'><span>
+        <FaLock/>
+        </span><span>Sign in</span>
+        <span className='opacity-0'>/</span>
+        </Button>
       </form>
+      </div>
     </div>
   )
 }
--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 function Provider({children}:{children:React.ReactNode}) {
   return (
     <div>
-      {children}
+      <SessionProvider>{children}</SessionProvider>
     </div>
   )
 }
