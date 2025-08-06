60b98b27a1cf5fccad0b5680d0985251d1c18339
anteneh83 <antenehgetnet83@gmail.com>
2025-08-05 22:47:53 -0900

    fix: authprovider missing session provider

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,13 +1,13 @@
-'use client'
-import { SessionProvider } from "next-auth/react"
-import React from 'react'
+"use client";
+import { SessionProvider } from "next-auth/react";
+import React from "react";
 
-function Provider({children}:{children:React.ReactNode}) {
+function Provider({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      {children}
+      <SessionProvider>{children}</SessionProvider>
     </div>
-  )
+  );
 }
 
-export default Provider
+export default Provider;
